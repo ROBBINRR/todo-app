3236ff7a28e5cd86e2f1ed6c31f4fa62ade4b039
ROBBINRR <roldan.1977@gmail.com>
2026-01-15 15:11:30 -0300

    Implement "Del new todo items" feature

--- web.py
+++ web.py
@@ -8,18 +8,25 @@ def add_todo():
     todos.append(to_do)
     functions.write_todos(todos)
 
+
 st.title("My Todo App")
 st.subheader("This is my todo app.")
 st.write("This app is to increase your productivity.")
 
-for todo in todos:
-    st.checkbox(todo)
+
+for index, todo in enumerate(todos):
+    checkbok = st.checkbox(todo, key=todo)
+    if checkbok:
+        todos.pop(index)
+        functions.write_todos(todos)
+        del st.session_state[todo]
+        st.rerun()
 
 st.text_input(label="", placeholder="Add new todo...",
               on_change=add_todo,key="new_todo")
 
-print("hello")
+#print("hello")
 
-st.session_state
+#st.session_state
 
 
